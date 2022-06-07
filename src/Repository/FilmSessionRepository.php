d6ea340aee691f378cb05f14f65d6144fab242f2
Anton Kunaev <kunaevanton25@gmail.com>
2022-06-07 14:29:31 +0300

    Make FilmSessionRepository

---
+++ src/Repository/FilmSessionRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repository;
+
+use App\Booking\Domain\Entity\FilmSession;
+use App\Booking\Domain\Repository\FilmSessionRepositoryInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+final class FilmSessionRepository extends ServiceEntityRepository implements FilmSessionRepositoryInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, FilmSession::class);
+    }
+
+    public function getById(string $id): FilmSession
+    {
+        return $this->find($id);
+    }
+
+    public function update(FilmSession $filmSession): void
+    {
+        $this->_em->persist($filmSession);
+        $this->_em->flush();
+    }
+}
