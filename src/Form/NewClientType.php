c30d0179a60c67c49e656f6d314b7eb0b9c5502c
Anton Kunaev <kunaevanton25@gmail.com>
2022-06-07 16:16:55 +0300

    Make form for client

---
+++ src/Form/NewClientType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Booking\Domain\TransferObject\NewClientDto;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class NewClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Имя',
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Телефон',
+            ])
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => NewClientDto::class,
+        ]);
+    }
+}
