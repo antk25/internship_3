1586feb69ab0928d6068d65299d5612e9c3e6c65
Anton Kunaev <kunaevanton25@gmail.com>
2022-06-08 09:26:38 +0300

    Remove unused code

--- src/Booking/Domain/Entity/ValueObject/Client.php
+++ src/Booking/Domain/Entity/ValueObject/Client.php
@@ -13,13 +13,8 @@ final class Client
     #[ORM\Column(type: 'bigint')]
     private string $phone;
 
-    /**
-     * @throws \Exception
-     */
     public function __construct(string $name, string $phone)
     {
-        self::assertThatNameIsValid($name);
-        self::assertThatPhoneIsValid($phone);
         $this->name = $name;
         $this->phone = $phone;
     }
@@ -32,25 +27,5 @@ final class Client
     public function getPhone(): string
     {
         return $this->phone;
-    }
-
-    /**
-     * @throws \Exception
-     */
-    private static function assertThatNameIsValid(string $name): void
-    {
-        if (!preg_match('/^[a-zA-Zа-яёА-ЯЁ]+$/u', $name)) {
-            throw new \Exception('Invalid name');
-        }
-    }
-
-    /**
-     * @throws \Exception
-     */
-    private static function assertThatPhoneIsValid(string $phone): void
-    {
-        if (!preg_match('/\+?[78][-(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/m', $phone)) {
-            throw new \Exception('Invalid phone format');
-        }
     }
 }
