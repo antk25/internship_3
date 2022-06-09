376ccd2aaff9d83235366b8b443c83e215d1c8af
Anton Kunaev <kunaevanton25@gmail.com>
2022-06-09 15:44:58 +0300

    Remove ManagerRegistry call from the CreateTicketHandler

--- src/Domain/Booking/Command/Handler/CreateTicketHandler.php
+++ src/Domain/Booking/Command/Handler/CreateTicketHandler.php
@@ -3,20 +3,15 @@
 namespace App\Domain\Booking\Command\Handler;
 
 use App\Domain\Booking\Command\CreateTicketCommand;
-use App\Domain\Booking\Entity\FilmSession;
-use App\Domain\Booking\Entity\Ticket;
-use App\Domain\Booking\Repository\TicketRepositoryInterface;
-use Doctrine\Persistence\ManagerRegistry;
+use App\Domain\Booking\Entity\ValueObject\Client;
+use App\Domain\Booking\Repository\FilmSessionRepositoryInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-use Symfony\Component\Uid\Uuid;
 
 #[AsMessageHandler]
 final class CreateTicketHandler
 {
-    public function __construct(
-        private readonly TicketRepositoryInterface $ticketRepository,
-        private readonly ManagerRegistry $registry,
-    ) {
+    public function __construct(private readonly FilmSessionRepositoryInterface $filmSessionRepository)
+    {
     }
 
     /**
