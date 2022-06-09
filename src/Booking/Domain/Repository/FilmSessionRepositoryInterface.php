13b6138ad22b4ee1fbb28e774dccb4843f4e528d
Anton Kunaev <kunaevanton25@gmail.com>
2022-06-09 10:52:36 +0300

    Fix returned type

--- src/Booking/Domain/Repository/FilmSessionRepositoryInterface.php
+++ src/Booking/Domain/Repository/FilmSessionRepositoryInterface.php
@@ -6,7 +6,7 @@ use App\Booking\Domain\Entity\FilmSession;
 
 interface FilmSessionRepositoryInterface
 {
-    public function findById(string $id): FilmSession;
+    public function findById(string $id): ?FilmSession;
 
     public function save(FilmSession $filmSession): void;
 }
