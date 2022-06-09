d688efe0171089d52fd690d0002fca87663803e8
Anton Kunaev <kunaevanton25@gmail.com>
2022-06-09 15:39:43 +0300

    Add fields to create a client in CreateTicketCommand

--- src/Domain/Booking/Command/CreateTicketCommand.php
+++ src/Domain/Booking/Command/CreateTicketCommand.php
@@ -3,13 +3,13 @@
 namespace App\Domain\Booking\Command;
 
 use App\Domain\Booking\Entity\FilmSession;
-use App\Domain\Booking\Entity\ValueObject\Client;
 
 final class CreateTicketCommand
 {
-    public function __construct(
-        public readonly FilmSession $filmSession,
-        public readonly Client $client,
-    ) {
+    public string $name;
+    public int $phone;
+
+    public function __construct(public readonly FilmSession $filmSession)
+    {
     }
 }
