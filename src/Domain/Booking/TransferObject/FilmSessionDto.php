116e4c7262678e2b26a2f961eeff6d2c6a6df6de
Anton Kunaev <kunaevanton25@gmail.com>
2022-06-10 15:50:45 +0300

    Create a filmSession without a service

--- src/Domain/Booking/TransferObject/FilmSessionDto.php
+++ src/Domain/Booking/TransferObject/FilmSessionDto.php
@@ -2,10 +2,27 @@
 
 namespace App\Domain\Booking\TransferObject;
 
+use phpDocumentor\Reflection\Type;
+
 final class FilmSessionDto
 {
     public string $filmName;
     public int $filmLength;
     public string $dateTimeStartFilmSession;
     public int $ticketsCount;
+
+    /**
+     * @param iterable<Type> $data
+     */
+    public function createFromArray(array $data): self
+    {
+        $dto = new self();
+
+        $dto->filmName = $data['film'];
+        $dto->filmLength = $data['filmLength'];
+        $dto->dateTimeStartFilmSession = $data['dateTimeStart'];
+        $dto->ticketsCount = $data['numberOfSeats'];
+
+        return $dto;
+    }
 }
