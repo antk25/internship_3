4843d991e51e8b3794b0640c2ad07f043b3a6dad
Anton Kunaev <kunaevanton25@gmail.com>
2022-06-10 08:03:25 +0300

    Move the validation to BookTicketCommand

--- src/Domain/Booking/Command/BookTicketCommand.php
+++ src/Domain/Booking/Command/BookTicketCommand.php
@@ -3,10 +3,24 @@
 namespace App\Domain\Booking\Command;
 
 use App\Domain\Booking\Entity\FilmSession;
+use Symfony\Component\Validator\Constraints as Assert;
 
 final class BookTicketCommand
 {
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 3,
+        max: 50,
+        minMessage: 'Длина вашего имени должна составлять не менее {{ limit }} символов',
+        maxMessage: 'Ваше имя не может быть длиннее {{ limit}} символов',
+    )]
     public string $name;
+
+    #[Assert\NotBlank]
+    #[Assert\Regex(
+        '/[78][-(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/m',
+        message: 'Неправильный формат номера телефона',
+    )]
     public int $phone;
 
     public function __construct(public readonly FilmSession $filmSession)
--- src/Domain/Booking/TransferObject/NewClientDto.php
+++ src/Domain/Booking/TransferObject/NewClientDto.php
@@ -2,23 +2,8 @@
 
 namespace App\Domain\Booking\TransferObject;
 
-use Symfony\Component\Validator\Constraints as Assert;
-
 final class NewClientDto
 {
-    #[Assert\NotBlank]
-    #[Assert\Length(
-        min: 3,
-        max: 50,
-        minMessage: 'Длина вашего имени должна составлять не менее {{ limit }} символов',
-        maxMessage: 'Ваше имя не может быть длиннее {{ limit}} символов',
-    )]
     public string $name;
-
-    #[Assert\NotBlank]
-    #[Assert\Regex(
-        '/[78][-(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/m',
-        message: 'Неправильный формат номера телефона',
-    )]
     public string $phone;
 }
