098fc0ada71394cfab8244b1bea13b8bab478319
Anton Kunaev <kunaevanton25@gmail.com>
2022-06-13 20:43:31 +0300

    Do validation in the Client

--- src/Domain/Booking/Entity/ValueObject/Client.php
+++ src/Domain/Booking/Entity/ValueObject/Client.php
@@ -13,9 +13,34 @@ final class Client
     #[ORM\Column(type: 'bigint')]
     private string $phone;
 
+    /**
+     * @throws \Exception
+     */
     public function __construct(string $name, string $phone)
     {
+        self::assertThatNameIsValid($name);
+        self::assertThatPhoneIsValid($phone);
         $this->name = $name;
         $this->phone = $phone;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private static function assertThatNameIsValid(string $name): void
+    {
+        if (!preg_match('/[A-zА-я-]{3,15}$/m', $name)) {
+            throw new \Exception('Invalid name');
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private static function assertThatPhoneIsValid(string $phone): void
+    {
+        if (!preg_match('/\+?[78][-(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/m', $phone)) {
+            throw new \Exception('Invalid phone format');
+        }
     }
 }
