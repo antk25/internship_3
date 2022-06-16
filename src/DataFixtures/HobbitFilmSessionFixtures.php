55798c384307020b1c5e677e0c1f70219f2cbd8f
Anton Kunaev <kunaevanton25@gmail.com>
2022-06-16 14:24:14 +0300

    Change the number of available seats in fixture

--- src/DataFixtures/HobbitFilmSessionFixtures.php
+++ src/DataFixtures/HobbitFilmSessionFixtures.php
@@ -16,7 +16,7 @@ final class HobbitFilmSessionFixtures extends Fixture
             'film' => 'Хоббит',
             'filmDuration' => 220,
             'dateTimeStart' => '22.06.2022 15:30',
-            'numberOfSeats' => 2,
+            'numberOfSeats' => 0,
         ];
 
         $filmSession = new FilmSession(
