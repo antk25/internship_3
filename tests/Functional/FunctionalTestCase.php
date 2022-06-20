547e0c191bf31c125fe15f8e33c896b83c7e4141
Anton Kunaev <kunaevanton25@gmail.com>
2022-06-20 15:52:09 +0300

    Declare a variable

--- tests/Functional/FunctionalTestCase.php
+++ tests/Functional/FunctionalTestCase.php
@@ -2,13 +2,16 @@
 
 namespace App\Tests\Functional;
 
+use Doctrine\Common\DataFixtures\Executor\AbstractExecutor;
 use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
 use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-abstract class AbstractFunctionalTestCase extends WebTestCase
+abstract class FunctionalTestCase extends WebTestCase
 {
     protected AbstractDatabaseTool $databaseTool;
+    protected KernelBrowser $client;
 
     /**
      * @throws \Exception
@@ -24,5 +27,13 @@ abstract class AbstractFunctionalTestCase extends WebTestCase
         parent::tearDown();
 
         unset($this->databaseTool);
+    }
+
+    /**
+     * @param array<string> $fixtures
+     */
+    public function loadFixtures(array $fixtures): AbstractExecutor
+    {
+        return $this->databaseTool->loadFixtures($fixtures);
     }
 }
